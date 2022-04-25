5f4a77ebf7ab2bc6af4cd2ff2392dd2a6b8dc393
Viraj <jadhavviraj50@gmail.com>
2022-04-25 22:53:06 +0530

    [Viraj]-Added FibonacciSeries File

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Select Number\n1)LogicalPrograms");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.fibonacci();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+
+                }
+            }
+        }
+    }
+}
