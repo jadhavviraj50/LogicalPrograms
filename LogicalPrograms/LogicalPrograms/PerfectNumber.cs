72b3a8054d9dc82438c4e1d4c82d91f41a1b26bb
Viraj <jadhavviraj50@gmail.com>
2022-04-25 22:58:52 +0530

    [Viraj]-Added PerfectNumber File

---
+++ LogicalPrograms/LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public void number()
+        {
+            {
+                int n, i, sum;
+                int a, b;
+                Console.Write("Input the starting range of number : ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Input the ending range of number : ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The Perfect numbers within the given range : ");
+                for (n = a; n <= b; n++)
+                {
+                    i = 1;
+                    sum = 0;
+                    while (i < n)
+                    {
+                        if (n % i == 0)
+                            sum = sum + i;
+                        i++;
+                    }
+                    if (sum == n)
+                        Console.Write("{0} ", n);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
+    
+
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -8,13 +8,17 @@ namespace LogicalPrograms
 
             while (true)
             {
-                Console.WriteLine("Select Number\n1)LogicalPrograms");
+                Console.WriteLine("Select Number\n1)LogicalPrograms\n2)PerfectNumber");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         FibonacciSeries series = new FibonacciSeries();
                         series.fibonacci();
+                        break;
+                    case 2:
+                        PerfectNumber perfect = new PerfectNumber();
+                        perfect.number();
                         break;
                     default:
                         Console.WriteLine("Wrong Input");
